534949b7814e2f7c1ee3a75d949617741542adaa
Anurag Maurya <90979669+AnuragMauryaa@users.noreply.github.com>
2026-08-02 02:02:33 +0530

    Delete components directory

--- components/components/Badge.jsx
+++
@@ -1,4 +0,0 @@
-// Small colored pill for statuses / movement types.
-export default function Badge({ color = 'gray', children }) {
-  return <span className={`badge ${color}`}>{children}</span>;
-}
