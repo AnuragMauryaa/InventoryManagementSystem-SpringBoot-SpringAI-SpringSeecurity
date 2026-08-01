f3c914af0490c3f08b9021e2d06096bd00688971
Anurag Maurya <90979669+AnuragMauryaa@users.noreply.github.com>
2026-08-02 01:50:10 +0530

    Add files via upload

---
+++ services/services/productService.js
@@ -0,0 +1,35 @@
+import api from "./api";
+
+const BASE = "/products";
+
+const getAll = async () => {
+    const response = await api.get(BASE);
+    return response.data;
+};
+
+const getById = async (id) => {
+    const response = await api.get(`${BASE}/${id}`);
+    return response.data;
+};
+
+const create = async (product) => {
+    const response = await api.post(BASE, product);
+    return response.data;
+};
+
+const update = async (id, product) => {
+    const response = await api.put(`${BASE}/${id}`, product);
+    return response.data;
+};
+
+const remove = async (id) => {
+    await api.delete(`${BASE}/${id}`);
+};
+
+export default {
+    getAll,
+    getById,
+    create,
+    update,
+    remove,
+};
