ba651e415d7ed708088dac661b8aeaf7da31f27a
Anurag Maurya <90979669+AnuragMauryaa@users.noreply.github.com>
2026-08-02 02:01:44 +0530

    Delete services directory

--- services/services/categoryService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const getAllCategories = async () => {
-    const response = await api.get("/categories");
-    return response.data;
-};
--- services/services/unitService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const getAllUnits = async () => {
-    const response = await api.get("/units");
-    return response.data;
-};
